f4df6e09c636130ae6bc976946c0573de17455d1
LuisMiguel <luismiguelescuderoalcaide10@gmail.com>
2026-06-16 11:41:55 +0200

    Arreglando el Boton de turnos

--- Assets/Scripts/Managers/BattleManager.cs
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14191dbf3997823d2219e88cb0d179bd5a68f2839ecf4e687e6c700ec7237c96
-size 17857
+oid sha256:76f1c3bf89b49ebe0f0d64c29aac42ecf4b6d49de6875d9e80895757a3678ed9
+size 18914
