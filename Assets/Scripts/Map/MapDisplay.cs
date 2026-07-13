ff56479c959f7d70ef701fe2ff4003270b8b506a
LuisMiguel <luismiguelescuderoalcaide10@gmail.com>
2026-07-13 22:00:36 +0200

    Arreglo Menores v1

--- Assets/Scripts/Map/MapDisplay.cs
+++ Assets/Scripts/Map/MapDisplay.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fe3131041e824190ad75aa2542eeb6c1231def33bc8dfdfef0bc9303f6743b3
-size 11836
+oid sha256:84eccfb5b58be9c06c172f84bb0e7650ed78b43355a2f71c673ff44a41dad253
+size 12442
